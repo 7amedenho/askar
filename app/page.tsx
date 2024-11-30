cfaa72cd3699df48b3311b7ec02f6736d27100d6
mo7amed7amedenho <mohamedhamed6611@gmail.com>
2024-11-30 12:06:24 +0200

    Fix Bugs Version 2.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const page = () => {
@@ -8,14 +7,3 @@ const page = () => {
 }
 
 export default page
-=======
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
-
-export default page
->>>>>>> 4979987308d4b074e09a564cd408dd341eb1c0a2
