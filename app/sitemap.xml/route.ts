e89aa173aff81d97706c93a8b7f79c413f132298
7amedenho <7amedenho@gmail.com>
2024-12-16 16:50:52 +0200

    Update Sitemap

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,39 +1,36 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const baseUrl = 'https://askarconstruction.com'; // ضع رابط موقعك الأساسي
+  const baseUrl = 'https://askarconstruction.com';
 
-  // الروابط المدعومة
+
   const pages = [
     { loc: '/en', lang: 'en' },
     { loc: '/ar', lang: 'ar' },
   ];
 
-  // إنشاء XML للـ Sitemap
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-            xmlns:xhtml="http://www.w3.org/1999/xhtml">
-      ${pages
-        .map(
-          (page) => `
-        <url>
-          <loc>${baseUrl}${page.loc}</loc>
-          <xhtml:link rel="alternate" hreflang="ar" href="${baseUrl}/ar" />
-          <xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en" />
-          <changefreq>daily</changefreq>
-          <priority>0.8</priority>
-        </url>
-      `
-        )
-        .join('')}
-    </urlset>
-  `;
 
-  // إرجاع الـ XML
-  return new NextResponse(sitemap, {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  ${pages
+    .map(
+      (page) => `
+  <url>
+    <loc>${baseUrl}${page.loc}</loc>
+    <xhtml:link rel="alternate" hreflang="ar" href="${baseUrl}/ar" />
+    <xhtml:link rel="alternate" hreflang="en" href="${baseUrl}/en" />
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>`
+    )
+    .join('')}
+</urlset>`;
+
+
+  return new NextResponse(sitemap.trim(), {
     headers: {
-      'Content-Type': 'application/xml',
+      'Content-Type': 'application/xml; charset=UTF-8',
     },
   });
 }
