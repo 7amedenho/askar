02f2ebf491dcef70d56a40c64364ae89432d979d
Mohamed Hamed <164372597+mo7amed7amedenho@users.noreply.github.com>
2025-04-22 19:42:47 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ده بيوقف التحقق من ESLint أثناء الـ build
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com', // النطاق المستخدم لخدمة الصور
-        pathname: '/**', // السماح بجميع المسارات على هذا النطاق
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
       },
     ],
   },
